8a7f7d5e8920766b747d19043e970f93d594ac2a
Jorge <mneyraarzapalo@gmail.com>
2025-09-25 09:11:50 -0500

    configuracion de estilos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+    }
         //gates en laravel
         Gate::define('ver-admin', function (User $user){
             return $user->rol === 'admin';
